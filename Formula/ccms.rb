23b2486c6166a6293549fccef1385f959fca1aa7
ReebanAustrive <madreeban277@gmail.com>
2026-09-03 17:15:54 +0530

    fix: checksum value error in v2.1.4

--- Formula/ccms.rb
+++ Formula/ccms.rb
@@ -3,7 +3,7 @@ class Ccms < Formula
   homepage "https://github.com/ReebanAustrive/AI-Assitant-Micro-Saas"
   version "2.1.4"
 
-  on_linux do
+ on_linux do
     url "https://github.com/ReebanAustrive/AI-Assitant-Micro-Saas/releases/download/v2.1.4/ccms-ubuntu-latest.zip"
     sha256 "6354ddc13f1d328b48e7d7cf49d9eed48a230b134564cd3eb7dee2d1a8f6bcd5"
   end
