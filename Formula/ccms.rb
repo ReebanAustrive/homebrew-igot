4b984019c21e37059390affa078e29d5c78228f3
ReebanAustrive <madreeban277@gmail.com>
2026-09-03 17:38:21 +0530

    Impl: new version

--- Formula/ccms.rb
+++ Formula/ccms.rb
@@ -1,10 +1,10 @@
 class Ccms < Formula
   desc "Agentic CLI Tool"
   homepage "https://github.com/ReebanAustrive/AI-Assitant-Micro-Saas"
-  version "2.1.4"
+  version "2.1.5"
 
  on_linux do
-    url "https://github.com/ReebanAustrive/AI-Assitant-Micro-Saas/releases/download/v2.1.4/ccms-ubuntu-latest.zip"
+    url "https://github.com/ReebanAustrive/AI-Assitant-Micro-Saas/releases/download/v2.1.5/ccms-ubuntu-latest.zip"
     sha256 "6354ddc13f1d328b48e7d7cf49d9eed48a230b134564cd3eb7dee2d1a8f6bcd5"
   end
 
